46642b65b478f2a9c82bcb96974b295333ee97a8
Ann <annagorova66@gmail.com>
2018-11-25 21:24:57 +0200

    sections 'reserve'

---
+++ src/js/reserve-btn.js
@@ -0,0 +1,17 @@
+let formmain = document.getElementById('reserve-main-click');
+let form = document.getElementById('reserve-click');
+
+function hideReserveBtn (event) {
+    if (event.target.tagName === 'INPUT'){
+        document.getElementById('res-btn').classList.add('display-none');
+    }
+    event.stopPropagation();
+}
+
+formmain.addEventListener('click', hideReserveBtn);
+form.addEventListener('click', hideReserveBtn);
+
+function showbtn(){
+    document.getElementById('res-btn').classList.remove('display-none');
+}
+document.body.addEventListener('click', showbtn, false);
