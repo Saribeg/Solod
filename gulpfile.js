8afa29c22f9852ae5b5bbe07c773c053a35876e2
yamnyk <notAunicorN2018>
2018-12-09 17:46:26 +0200

    gulp-file asycnronic chronology fixed

--- gulpfile.js
+++ gulpfile.js
@@ -151,7 +151,7 @@ gulp.task('clean', function () {
 
 
 gulp.task('default', function(){
-    runSequence('clean', ['build', 'watch']), function () {
+    runSequence('clean', 'build', 'watch'), function () {
     console.log('===ALL DONE===')
     }
 });
