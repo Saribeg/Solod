82cfc5d959bef5bca86b4f141de559634f107680
yamnyk <notAunicorN2018>
2018-12-10 06:27:09 +0200

    sticky btn issue resolved

--- src/js/reserve.js
+++ src/js/reserve.js
@@ -1,6 +1,13 @@
 //reserve-btn
 let formmain = $('#reserve-main-click');
 let form = $('#reserve-click');
+let time = {
+    hours: new Date().getHours() < 10 ? `0${new Date().getHours() + 2}` : `${new Date().getHours() + 2}`,
+    minutes: new Date().getMinutes() < 10 ? `0${new Date().getMinutes()}` : new Date().getMinutes(),
+};
+console.log(`${time.hours}:${time.minutes}`);
+
+$('#reserve-time')[0].defaultValue = `${time.hours}:${time.minutes}`;
 
 $('#reserve-date')[0].valueAsDate = new Date();
 
@@ -14,7 +21,7 @@ function hideReserveBtn (event) {
 formmain.on('click', hideReserveBtn);
 form.on('click', hideReserveBtn);
 
-function showbtn(){
+function showbtn(event){
     $('#res-btn').removeClass('display-none');
 }
 $(document.body).on('click', showbtn);
