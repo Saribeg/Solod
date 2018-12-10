3583d27935a02eb5edbe9037d58023658f03231b
yamnyk <notAunicorN2018>
2018-12-10 04:43:19 +0200

    adaptive fixing

--- src/js/reserve.js
+++ src/js/reserve.js
@@ -1,20 +1,20 @@
 //reserve-btn
-let formmain = document.getElementById('reserve-main-click');
-let form = document.getElementById('reserve-click');
+let formmain = $('#reserve-main-click');
+let form = $('#reserve-click');
 
 $('#reserve-date')[0].valueAsDate = new Date();
 
 function hideReserveBtn (event) {
     if (event.target.tagName === 'INPUT'){
-        document.getElementById('res-btn').classList.add('display-none');
+        $('#res-btn').addClass('display-none');
     }
     event.stopPropagation();
 }
 
-formmain.addEventListener('click', hideReserveBtn);
-form.addEventListener('click', hideReserveBtn);
+formmain.on('click', hideReserveBtn);
+form.on('click', hideReserveBtn);
 
 function showbtn(){
-    document.getElementById('res-btn').classList.remove('display-none');
+    $('#res-btn').removeClass('display-none');
 }
-document.body.addEventListener('click', showbtn, false);
+$(document.body).on('click', showbtn);
