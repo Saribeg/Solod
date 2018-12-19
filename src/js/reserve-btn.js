49bc33081a6516cb530ebf6a9f4d95849a6e09a3
Ann <annagorova66@gmail.com>
2018-12-19 21:56:52 +0200

    add scroll for btn-reserve

--- src/js/reserve-btn.js
+++ src/js/reserve-btn.js
@@ -1,18 +1,10 @@
 //reserve-btn
-let formmain = document.getElementById('reserve-main-click');
-let form = document.getElementById('reserve-click');
 
-function hideReserveBtn (event) {
-    if (event.target.tagName === 'INPUT'){
-        document.getElementById('res-btn').classList.add('display-none');
-    }
-    event.stopPropagation();
-}
-
-formmain.addEventListener('click', hideReserveBtn);
-form.addEventListener('click', hideReserveBtn);
-
-function showbtn(){
-    document.getElementById('res-btn').classList.remove('display-none');
-}
-document.body.addEventListener('click', showbtn, false);
+$(document).ready(function () {
+    $("#res-btn").on("click", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
