9d05262e850e562b5ee05f3a3dca0f270f6b7cbe
yamnyk <notAunicorN2018>
2018-12-19 14:51:08 +0200

    footer | white version works, add JS white folder

---
+++ src/js/white/menu-page-white.js
@@ -0,0 +1,9 @@
+@@include('../modules/_nav-menu.js');
+@@include('../modules/_reserve.js');
+@@include('../modules/_tab-menu.js');
+@@include('../modules/_map-white.js');
+
+_navMenu();
+reserveBtn(true);
+tabMenu();
+initMap();
