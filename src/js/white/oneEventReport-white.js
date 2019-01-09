dc8504063a8ebe8fbcf9ef650b422f7cd2e74287
Tim <luckystudydanit@gmail.com>
2019-01-09 14:32:07 +0200

    09/01/19 fixed all bugs

---
+++ src/js/white/oneEventReport-white.js
@@ -0,0 +1,11 @@
+@@include('../modules/_nav-menu.js');
+@@include('../modules/_slow-scroll.js');
+@@include('../modules/_reserve.js');
+@@include('../modules/_map-white.js');
+@@include('../modules/_photo-slider.js');
+
+_navMenu();
+slowScroll('#res-btn');
+slowScroll('#contacts-btn');
+reserveBtn(true);
+initMap();
