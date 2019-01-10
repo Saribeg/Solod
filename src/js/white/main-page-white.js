0277aaf9b900957d1d6fa8ced3a9f8b4d070df45
yamnyk <notAunicorN2018>
2019-01-10 03:10:58 +0200

    reserve-from white| add custom datepicker

--- src/js/white/main-page-white.js
+++ src/js/white/main-page-white.js
@@ -1,10 +1,13 @@
 @@include('../modules/_nav-menu.js');
 @@include('../modules/_reserve.js');
+@@include('../modules/_slow-scroll.js');
 @@include('../modules/_mainpage_menu.js');
 @@include('../modules/_slick-slider.js');
 @@include('../modules/_map-white.js');
 
 _navMenu();
+slowScroll('#res-btn');
+slowScroll('#contacts-btn');
 reserveBtn(false);
 mainPageMenu();
 $(slickSlider());
