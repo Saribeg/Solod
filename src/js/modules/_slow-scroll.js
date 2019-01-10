252f84e35c3dcdbfd86c76040baaacab7ed756be
yamnyk <notAunicorN2018>
2019-01-10 15:28:14 +0200

    add decoration arrow to date inputs

--- src/js/modules/_slow-scroll.js
+++ src/js/modules/_slow-scroll.js
@@ -4,6 +4,7 @@ function slowScroll(id) {
         event.preventDefault();
         $('.tablet-nav-menu__list').hide();
         $('.tablet-nav-menu__burger').toggleClass('burger_toggle');
+        $('.mobile-nav-menu__list').fadeOut();
         $(".mobile-nav-menu__burger").toggleClass("burger_toggle");
         let scrollTo = $(this).attr('href'),
             top = $(scrollTo).offset().top;
