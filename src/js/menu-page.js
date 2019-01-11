d076d4b11e3321d9fd404439dabd29411856bea7
yamnyk <notAunicorN2018>
2019-01-11 17:40:15 +0200

    tab-menu | rewrite tabs from scratch. Only dropdown works bad

--- src/js/menu-page.js
+++ src/js/menu-page.js
@@ -4,6 +4,10 @@
 @@include('modules/_tab-menu.js');
 @@include('modules/_map-dark.js');
 
+$('#my-tabs').jtabs({
+    storage: 'localstorage'
+});
+
 _navMenu();
 slowScroll('#res-btn');
 slowScroll('#contacts-btn');
